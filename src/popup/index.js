f905c98f8f587814c5403cb5c8e5af1ec0e5718c
billyzou <yz3352@nyu.edu>
2019-02-11 18:42:30 -0500

    feat: add item to storage(not completed yet)

--- src/popup/index.js
+++ src/popup/index.js
@@ -13,15 +13,67 @@ function getCurrentIcon() {
     return getCurrentTab().then(currTab => currTab.favIconUrl);
 }
 
-async function main() {
-    // get DOM elements
-    const icon = document.querySelector('.header__icon');
+async function getIdCounter() {
+    const idCounter = await browser.storage.local.get('idCounter');
+    if (idCounter.idCounter === undefined) {
+        await browser.storage.local.set({
+            idCounter: 0,
+        });
+        return 0;
+    }
+    return idCounter.idCounter;
+}
 
-    // display current icon in popup
+async function increaseIdCounter() {
+    const idCounter = await browser.storage.local.get('idCounter');
+    await browser.storage.local.set({
+        idCounter: idCounter.idCounter + 1,
+    });
+}
+
+async function getList() {
+    const list = await browser.storage.local.get('list');
+    return list;
+}
+
+async function saveList(list) {
+    console.log(list);
+    await browser.storage.local.set({ list });
+}
+
+async function updateAndSaveList(id, content) {
+    const list = await getList();
+    list.list[id] = content;
+    await saveList(list);
+    await increaseIdCounter();
+}
+
+// add a new to-do item to storage
+function addNewToDoItem() {
+    const newContent = document.getElementById('new-content');
+    const newSubmit = document.getElementById('new-submit');
+
+    newSubmit.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const idCounter = await getIdCounter();
+        const content = newContent.value;
+        await updateAndSaveList(idCounter, content);
+    });
+}
+
+// display current icon in popup
+async function updateIcon() {
+    const icon = document.querySelector('.header__icon');
     const currIcon = await getCurrentIcon();
     if (currIcon) {
         icon.src = currIcon;
     }
+}
+
+async function main() {
+    addNewToDoItem();
+    await updateIcon();
+    await getIdCounter();
 }
 
 main();
